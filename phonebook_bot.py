5f90d805abebe5720671d37f002fd279ae859f98
Gene4kaS <80684066+Gene4kaS@users.noreply.github.com>
2023-03-22 11:24:48 +0000

    Update phonebook_bot.py

--- phonebook_bot.py
+++ phonebook_bot.py
@@ -1,4 +1,5 @@
 from collections import UserDict
+from datetime import datetime
 
 
 class Field:
@@ -10,13 +11,34 @@ class Name(Field):
     pass
 
 class Phone(Field):
-    pass
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if not all((value.startswith('+'), len(value) == 13, value[1:].isdigit())):
+            raise ValueError("Введіть номер в форматі '+380111111111'")
+        self.__value = value
 
 
 class Record:
-    def __init__(self, name):
+    def __init__(self, name, phone, birthday):
         self.name = Name(name)
-        self.phones = []
+
+        if birthday:
+            self.birthday = Birthday(birthday)
+        else:
+            self.birthday = ''
+
+        if phone:
+            self.phones = [Phone(phone)]
+        else:
+            self.phones = []
 
     def get_info(self):
         for phon in self.phones:
@@ -31,7 +53,6 @@ class Record:
             if not self.delete_phone(phone):
                 self.add_phone(phone)
 
-
     def delete_phone(self, phone):
         for record_phone in self.phones:
             if record_phone.value == phone:
@@ -39,8 +60,64 @@ class Record:
                 return True
         return False
 
+    def days_to_birthday(self):
+        date_now = datetime.now()
+        try:
+            date_birthday = datetime(year=date_now.year, month=self.month, day=self.day)
+        except:
+            date_birthday = datetime(year=date_now.year, month=self.month, day=self.day - 1)
+
+        if date_birthday < date_now:
+            try:
+                date_birthday = datetime(year=date_now.year + 1, month=self.month, day=self.day)
+            except:
+                date_birthday = datetime(year=date_now.year + 1, month=self.month, day=self.day - 1)
+
+        return (date_birthday - date_now).days
+
+class Birthday(Field):
+
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+
+    def __str__(self) -> str:
+        return datetime.strftime(self.__value, '%d.%m.%Y')
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        try:
+            date_birthday = datetime.strptime(value, '%d.%m.%Y')
+            self.__value = date_birthday
+        except:
+            raise ValueError("Введіть день народження в форматі 'дд.мм.ггг' (наприклад 01.01.1990)")
 
 class AddressBook(UserDict):
+    N = 10
+    current_index = 0
+
+    def iterator(self):
+        names = sorted([name for name in self.data])
+        while AddressBook.current_index < len(self.data):
+            text_records = ''
+            for name in names[AddressBook.current_index:min(len(self.data), AddressBook.current_index + AddressBook.N)]:
+                record = self.data[name]
+                record_name = record.name.value
+                record_birthday = str(record.birthday)
+                for phone in record.phones:
+                    text_records += '{:<20}{:>20}{:>20}\n'.format(record_name, phone.value, record_birthday)
+                    record_name = ''
+                    record_birthday = ''
+                if record_name:
+                    text_records += '{:<20}{:>20}{:>20}\n'.format(record_name, '', record_birthday)
+
+            yield text_records
+            AddressBook.current_index += AddressBook.N
+
     def add_record(self, record):
         self.data[record.name.value] = record
 
